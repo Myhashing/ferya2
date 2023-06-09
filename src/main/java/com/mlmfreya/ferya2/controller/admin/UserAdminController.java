9b594165bae0ac265092712e61f61b78de7c9435
Reza <arianpourinfo@gmail.com>
2023-06-09 14:39:47 +0330

    remvoe test

--- src/main/java/com/mlmfreya/ferya2/controller/admin/UserAdminController.java
+++ src/main/java/com/mlmfreya/ferya2/controller/admin/UserAdminController.java
@@ -3,6 +3,7 @@ package com.mlmfreya.ferya2.controller.admin;
 import com.mlmfreya.ferya2.model.User;
 import com.mlmfreya.ferya2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -32,12 +33,18 @@ public class UserAdminController {
     }
 
     @PostMapping("/{id}")
-    public String updateUser(@PathVariable Long id, @ModelAttribute User updatedUser) {
+    @ResponseBody
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
         return userRepository.findById(id)
                 .map(user -> {
                     user.setEmail(updatedUser.getEmail());
                     user.setFullName(updatedUser.getFullName());
-                    user.setPassword(updatedUser.getPassword());
+                    if (!updatedUser.getPassword().isEmpty()) {
+                        String encodedPassword = passwordEncoder.encode(updatedUser.getPassword());
+                        user.setPassword(encodedPassword);
+                    }
                     user.setRole(updatedUser.getRole());
                     user.setTronWalletAddress(updatedUser.getTronWalletAddress());
                     user.setReferralCode(updatedUser.getReferralCode());
@@ -45,10 +52,10 @@ public class UserAdminController {
                     user.setEmailVerificationToken(updatedUser.getEmailVerificationToken());
                     user.setEmailVerified(updatedUser.isEmailVerified());
                     user.setBalance(updatedUser.getBalance());
-                    userRepository.save(user);
-                    return "redirect:/admin/user/list";  // redirect back to the user list
+                    return userRepository.save(user);
                 })
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
     }
+
 
 }
